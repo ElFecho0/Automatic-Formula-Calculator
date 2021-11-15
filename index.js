a814c501a67df02d5e937536bed681969126fe20
ElFecho0 <fflores.monsivais@gmail.com>
2021-11-15 21:15:51 +0000

    Optimización y inclusión dde más formulas

--- index.js
+++ index.js
@@ -1,43 +1,47 @@
-console.log("Teorema de Pitágoras:");
-//a^2 + b^2 = c^2
-//c = raíz de(a^2 + b^2)
-function pitagoras(a,b){
-    a2 = Math.pow(a,2);
-    b2 = Math.pow(b,2);
+function pitagorasA(c,b){
+    console.log("Teorema de Pitágoras: Hipotenusa");
+    //a^2 + b^2 = c^2
+    //a = sqrt(c^2 - b^2)
 
-    suma = a2+b2;
-
-    resultado = Math.sqrt(suma)
+    resultado = Math.sqrt(Math.pow(c,2) - Math.pow(b,2));
 
     console.log("La hipotenusa tiene un valor de " + resultado)
 }
-//acá van los catetos
-pitagoras(20,21);
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function pitagorasB(c,a){
+    console.log("Teorema de Pitágoras: Hipotenusa");
+    //a^2 + b^2 = c^2
+    //b = sqrt(c^2 - a^2)
 
-console.log();
+    resultado = Math.sqrt(Math.pow(c,2) - Math.pow(a,2));
 
-console.log("Función General de las ecuaciones Cuadráticas:");
-//ax^2 + bx + c = 0
+    console.log("La hipotenusa tiene un valor de " + resultado)
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function pitagorasC(a,b){
+    console.log("Teorema de Pitágoras: Hipotenusa");
+    //a^2 + b^2 = c^2
+    //c = sqrt(a^2 + b^2)
+
+    resultado = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+    console.log("La hipotenusa tiene un valor de " + resultado)
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function ecuacionCuardatica(a,b,c){
-    //antes del +-
-    antes = -b;
+    console.log("Función General de las ecuaciones Cuadráticas:");
+    //ax^2 + bx + c = 0
+    // -b +- sqrt(b^2 -4ac)/2a
 
     //raiz cuadrada
-    radicando = Math.pow(b,2) - 4 * a * c;
-    raiz = Math.sqrt(radicando);
-
-    //denominador
-    denominador = 2 * a;
+    raiz = Math.sqrt(Math.pow(b,2) - 4 * a * c);
 
     //resultados finales
-    resultado1 = (antes + raiz) / denominador;
-    resultado2 = (antes - raiz) / denominador;
+    resultado1 = (-b + raiz) / 2 * a;
+    resultado2 = (-b - raiz) / 2 * a;
 
     //excepción
     if (a == 0) {
-        console.log("x1 es Nulo");
-        console.log("x2 es Nulo");
-        console.log();
         console.log("!!!!!!ERROR!!!!!!");
         console.log('"a" no puede valer 0');
     }else{
@@ -45,5 +49,17 @@ function ecuacionCuardatica(a,b,c){
         console.log("x2 es " + resultado2);
     }
 }
+
+//acá van los catetos
+
+//           c,b
+//pitagorasA(3,4);
+
+//           c,a
+//pitagorasB(3,4);
+
+//           a,b
+//pitagorasC(3,4);
+
 //asignar los valores de a,b y c
-ecuacionCuardatica(1,-5,-84);
+//ecuacionCuardatica(1,-7,-18);
